5454573637f5f011d6347aea187dfef48a32b008
Christopher Brown <cjbrown1@ksu.edu>
2022-09-20 11:04:09 -0500

    KindleApp Solution Created

---
+++ KindleApp/KindleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KindleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
